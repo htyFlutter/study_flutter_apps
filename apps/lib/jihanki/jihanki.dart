ac6e87226d36b7ca687f932aebeb38b0d19844cf
Hayato <7hyt0512@gmail.com>
2026-05-28 14:48:32 +0900

    2026-05-28

--- apps/lib/jihanki/jihanki.dart
+++ apps/lib/jihanki/jihanki.dart
@@ -12,11 +12,14 @@ class _JihankiState extends State<Jihanki> {
   String costomerBoughtJuice = '';
   bool didCostomerBought = false;
   bool coustomerPutIn = false;
+  String errorMessage = '';
 
-  void initstate() {
+  @override
+  void initState() {
     super.initState();
   }
 
+  //100円を入れる
   void hundredYenPutIn() {
     setState(() {
       totalputinYen += 100;
@@ -24,13 +27,16 @@ class _JihankiState extends State<Jihanki> {
     });
   }
 
+  //50円を入れる
   void fiftyYenPutIn() {
     setState(() {
       totalputinYen += 50;
       coustomerPutIn = true;
+      errorMessage = '';
     });
   }
 
+  //コカコーラ（砂糖入り）を買う関数
   void cocaCokeNormalBought() {
     setState(() {
       totalputinYen -= 160;
@@ -38,6 +44,7 @@ class _JihankiState extends State<Jihanki> {
     });
   }
 
+  //コカコーラゼロを買う関数
   void cocaColaZeroBought() {
     setState(() {
       totalputinYen -= 160;
@@ -45,6 +52,7 @@ class _JihankiState extends State<Jihanki> {
     });
   }
 
+  //コカコーラトクホを買う関数
   void cocaColaTokuhoBought() {
     setState(() {
       totalputinYen -= 200;
@@ -54,14 +62,39 @@ class _JihankiState extends State<Jihanki> {
 
   void moneyless() {
     if (totalputinYen < 0) {
-      const Text(
-        'お金が足りません。',
-        style: TextStyle(fontSize: 30, color: Colors.redAccent),
-      );
+      errorMessage = 'お金が足りません。';
       didCostomerBought = false;
     }
   }
 
+  //お釣りを出す
+  void oturiout() {
+    if (totalputinYen > 0) {
+      setState(() {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('お釣り返却💰', textAlign: TextAlign.center),
+              content: Text('お釣り$totalputinYen円です。'),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('確認'),
+                ),
+              ],
+            );
+          },
+        );
+
+        totalputinYen = 0;
+        errorMessage = '';
+      });
+    }
+  }
+
+  //自販機のお金をを入れる時からコーラを受け取るまでの流れ
+  //上の看板
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,15 +105,30 @@ class _JihankiState extends State<Jihanki> {
           style: TextStyle(color: Colors.black, fontSize: 18),
         ),
       ),
+
+      //自販機の配置
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          //商品のボタン
           Column(
             children: [
+              Text('現在の投入額: $totalputinYen円', style: TextStyle(fontSize: 30)),
+              SizedBox(height: 20),
+
+              if (errorMessage.isNotEmpty)
+                Text(
+                  errorMessage,
+                  style: TextStyle(
+                    color: Colors.redAccent,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              const SizedBox(height: 20),
+
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                  fixedSize: Size(double.infinity, 10),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
@@ -88,11 +136,13 @@ class _JihankiState extends State<Jihanki> {
                 ),
 
                 onPressed: () {
-                  if (totalputinYen <= 0) {
-                    const Text('お金が足りません。');
-                    moneyless();
-                  } else {
+                  if (totalputinYen >= 160) {
                     cocaCokeNormalBought();
+                    errorMessage = '';
+                  } else {
+                    setState(() {
+                      errorMessage = 'お金が足りません。';
+                    });
                   }
                 },
                 child: const Text(
@@ -101,6 +151,7 @@ class _JihankiState extends State<Jihanki> {
                 ),
               ),
               SizedBox(height: 10),
+
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   fixedSize: Size(double.infinity, 10),
@@ -110,11 +161,13 @@ class _JihankiState extends State<Jihanki> {
                   backgroundColor: Colors.black,
                 ),
                 onPressed: () {
-                  if (totalputinYen <= 0) {
-                     const Text('お金が足りません。');
-                    moneyless();
-                  } else {
+                  if (totalputinYen >= 160) {
                     cocaColaZeroBought();
+                    errorMessage = '';
+                  } else {
+                    setState(() {
+                      errorMessage = 'お金が足りません。';
+                    });
                   }
                 },
                 child: const Text(
@@ -135,11 +188,13 @@ class _JihankiState extends State<Jihanki> {
                   backgroundColor: Colors.white,
                 ),
                 onPressed: () {
-                  if (totalputinYen <= 0) {
-                    const Text('お金が足りません。');
-                    moneyless();
-                  } else {
+                  if (totalputinYen >= 200) {
                     cocaColaTokuhoBought();
+                    errorMessage = '';
+                  } else {
+                    setState(() {
+                      errorMessage = 'お金が足りません。';
+                    });
                   }
                 },
                 child: const Text(
@@ -150,6 +205,8 @@ class _JihankiState extends State<Jihanki> {
               SizedBox(height: 20),
             ],
           ),
+
+          //お金を入れるボタン
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -168,15 +225,16 @@ class _JihankiState extends State<Jihanki> {
               ),
             ],
           ),
+
+          //お釣りを出す
           Center(
             child: ElevatedButton(
               style: ElevatedButton.styleFrom(
-                fixedSize: Size(double.infinity, 10),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
               ),
-              onPressed: () {},
+              onPressed: oturiout,
               child: const Text('おつりを出す'),
             ),
           ),
--- apps/lib/main.dart
+++ apps/lib/main.dart
@@ -1,7 +1,7 @@
-
+import 'package:counter_apps/quiz/list_practice.dart';
+import 'package:marquee/marquee.dart';
 import 'package:counter_apps/jihanki/jihanki.dart';
 import 'package:flutter/material.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -12,8 +12,73 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Jihanki(), 
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Many Kinds of Apps',
+      theme: ThemeData(primaryColor: Colors.redAccent),
+      home: MainNavigationScreen(),
+    );
+  }
+}
+
+class MainNavigationScreen extends StatefulWidget {
+  const MainNavigationScreen({super.key});
+
+  @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [const Jihanki(), const ListPractice()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            color: Colors.black,
+            height: 30,
+            child: Marquee(
+              text: 'コカコータ専用自販機へようこそ！つめた〜いコーラ販売中！　お釣りボタンを押すと残高が表示されます。' ,
+              style: const TextStyle(
+                color: Colors.redAccent, 
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+              scrollAxis: Axis.horizontal,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              blankSpace: 50.0,
+              velocity: 50.0,
+              pauseAfterRound: const Duration(seconds: 3),
+            ),
+          ),
+
+          BottomNavigationBar(
+            currentIndex: _selectedIndex,
+            onTap: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.local_drink),
+                label: '自販機',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.book),
+                label: 'クイズ',
+              ),
+            ],
+            type: BottomNavigationBarType.fixed,
+          )
+        ],
+      ),
     );
   }
 }
